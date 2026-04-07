7590cc22da1881b279deba7e317fc2b8357b8b37
Alexander <18-a.korenev@users.noreply.gitlab.rocketnetwork.site>
2026-04-07 14:03:52 +0200

    fix(ui): cache hashed assets and slim design fonts

--- src/designs/font-loaders.ts
+++ src/designs/font-loaders.ts
@@ -1,60 +1,29 @@
 export const loadBrutalFonts = () =>
-  Promise.all([
-    import("@fontsource/archivo-black/latin-400.css"),
-    import("@fontsource/space-mono/latin-400.css"),
-    import("@fontsource/space-mono/latin-700.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/brutal.css")]).then(() => undefined);
 
 export const loadAuroraFonts = () =>
-  Promise.all([import("@fontsource-variable/outfit/wght.css")]).then(() => undefined);
+  Promise.all([import("./fonts/outfit.css")]).then(() => undefined);
 
 export const loadTerminalFonts = () =>
-  Promise.all([
-    import("@fontsource/vt323/latin-400.css"),
-    import("@fontsource-variable/fira-code/wght.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/terminal.css")]).then(() => undefined);
 
 export const loadEditorialFonts = () =>
-  Promise.all([
-    import("@fontsource-variable/playfair-display/wght.css"),
-    import("@fontsource-variable/playfair-display/wght-italic.css"),
-    import("@fontsource-variable/cormorant-garamond/wght.css"),
-    import("@fontsource-variable/cormorant-garamond/wght-italic.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/editorial.css")]).then(() => undefined);
 
 export const loadNeonFonts = () =>
-  Promise.all([
-    import("@fontsource/rajdhani/latin-400.css"),
-    import("@fontsource/rajdhani/latin-500.css"),
-    import("@fontsource-variable/orbitron/wght.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/neon.css")]).then(() => undefined);
 
 export const loadHustleFonts = () =>
-  Promise.all([
-    import("@fontsource-variable/outfit/wght.css"),
-    import("@fontsource/archivo-black/latin-400.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/hustle.css")]).then(() => undefined);
 
 export const loadDreamyFonts = () =>
-  Promise.all([import("@fontsource-variable/outfit/wght.css")]).then(() => undefined);
+  Promise.all([import("./fonts/outfit.css")]).then(() => undefined);
 
 export const loadShadowFonts = () =>
-  Promise.all([
-    import("@fontsource/rajdhani/latin-400.css"),
-    import("@fontsource/rajdhani/latin-500.css"),
-    import("@fontsource/rajdhani/latin-600.css"),
-    import("@fontsource/rajdhani/latin-700.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/shadow.css")]).then(() => undefined);
 
 export const loadPrestigeFonts = () =>
-  Promise.all([
-    import("@fontsource-variable/cormorant-garamond/wght.css"),
-    import("@fontsource-variable/cormorant-garamond/wght-italic.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/prestige.css")]).then(() => undefined);
 
 export const loadMoonFonts = () =>
-  Promise.all([
-    import("@fontsource/rajdhani/latin-400.css"),
-    import("@fontsource/rajdhani/latin-500.css"),
-    import("@fontsource-variable/orbitron/wght.css"),
-  ]).then(() => undefined);
+  Promise.all([import("./fonts/moon.css")]).then(() => undefined);
